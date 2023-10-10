836f4f06329bd0967f3f556ec3ef6de7be2301a4
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-10-10 17:48:28 -0300

    feat: add validações dos campos do dto CreateUserRequest

--- news-api/src/main/java/br/com/emendes/newsapi/dto/request/CreateUserRequest.java
+++ news-api/src/main/java/br/com/emendes/newsapi/dto/request/CreateUserRequest.java
@@ -1,16 +1,26 @@
 package br.com.emendes.newsapi.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 /**
  * Record DTO para receber os dados de criação de User.
  */
-// TODO: adicionar as validações de dados.
 @Builder
 public record CreateUserRequest(
+    @NotBlank(message = "name must not be blank")
+    @Size(min = 2, max = 50, message = "name must contain between {min} and {max} characters long")
     String name,
+    @NotBlank(message = "email must not be blank")
+    @Size(max = 320, message = "email must contain {max} characters long")
+    @Email(message = "email must be a well formed E-mail")
     String email,
+    @NotBlank(message = "password must not be blank")
+    @Size(min = 8, max = 30, message = "password must contain between {min} and {max} characters long")
     String password,
+    @NotBlank(message = "confirmPassword must not be blank")
     String confirmPassword
 ) {
 }
