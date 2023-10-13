a95a1942173f8c7ed36f3bf2ea4c9b0da73cc055
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-10-13 16:24:09 -0300

    fix: add handler para MethodArgumentNotValidException

--- news-api/src/main/java/br/com/emendes/newsapi/handler/GlobalExceptionHandler.java
+++ news-api/src/main/java/br/com/emendes/newsapi/handler/GlobalExceptionHandler.java
@@ -1,20 +1,44 @@
 package br.com.emendes.newsapi.handler;
 
 import br.com.emendes.newsapi.exception.UserCreationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.stream.Collectors;
 
 /**
  * Controller advice responsável por lidar com exceptions que ocorrerem a partir dos controllers.
  */
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    String fields = exception.getFieldErrors().stream()
+        .map(FieldError::getField).collect(Collectors.joining("; "));
+    String messages = exception.getFieldErrors().stream()
+        .map(FieldError::getDefaultMessage).collect(Collectors.joining("; "));
+
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatusCode.valueOf(400));
+
+    problemDetail.setTitle("Invalid fields");
+    problemDetail.setDetail("Some fields are invalids");
+    problemDetail.setProperty("fields", fields);
+    problemDetail.setProperty("messages", messages);
+    problemDetail.setType(URI.create("https://github.com/Edson-Mendes/news-api/blob/main/README.md"));
+
+    return ResponseEntity.badRequest().body(problemDetail);
+  }
 
   @ExceptionHandler(UserCreationException.class)
   public ResponseEntity<ProblemDetail> handleUserCreation(UserCreationException exception) {
