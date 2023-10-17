c127aeef9854e0a4fffad578e662b1265fcfeb89
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-10-17 13:29:40 -0300

    config: add beans relacionados a camada de security

---
+++ news-api/src/main/java/br/com/emendes/newsapi/config/beans/SecurityBeans.java
@@ -0,0 +1,56 @@
+package br.com.emendes.newsapi.config.beans;
+
+import br.com.emendes.newsapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Classe com os beans relacionados a Security.
+ */
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SecurityBeans {
+
+  private final UserRepository userRepository;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService());
+    authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+    return authenticationProvider;
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    return username -> {
+      log.info("Searching for user with email : {}", username);
+
+      return userRepository.findByEmailAndEnabledTrue(username)
+          .orElseThrow(() -> new UsernameNotFoundException(String.format("User %s not found", username)));
+    };
+  }
+
+}
--- news-api/src/main/java/br/com/emendes/newsapi/repository/UserRepository.java
+++ news-api/src/main/java/br/com/emendes/newsapi/repository/UserRepository.java
@@ -2,9 +2,21 @@ package br.com.emendes.newsapi.repository;
 
 import br.com.emendes.newsapi.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 /**
  * Interface repository com as abstrações para persistência do recurso User.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  /**
+   * Busca um usuário por username (email).
+   *
+   * @param username email do usuário a ser buscado.
+   * @return {@code Optional<UserDetails>}.
+   */
+  Optional<UserDetails> findByEmailAndEnabledTrue(String username);
+
 }
