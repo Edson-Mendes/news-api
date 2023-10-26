7e29ec9d8939c846546e59cff9bb82f30153dfdf
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-10-26 14:45:54 -0300

    feat: notification app recebe os dados de confirmação de conta

---
+++ notification/src/main/java/br/com/emendes/notification/dto/ConfirmationNotificationDTO.java
@@ -0,0 +1,21 @@
+package br.com.emendes.notification.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
+import java.net.URI;
+
+/**
+ * Record DTO sobre notificação.
+ */
+public record ConfirmationNotificationDTO(
+    @NotBlank(message = "addresseeEmail must not be blank")
+    @Email(message = "addresseeEmail must be a well formed E-mail")
+    String addresseeEmail,
+    @NotBlank(message = "content must not be blank")
+    String content,
+    @URL(message = "confirmationURI must be a well formed URL")
+    URI confirmationURI
+) {
+}
--- notification/src/main/java/br/com/emendes/notification/listener/NotificationListener.java
+++ notification/src/main/java/br/com/emendes/notification/listener/NotificationListener.java
@@ -1,6 +1,6 @@
 package br.com.emendes.notification.listener;
 
-import br.com.emendes.notification.dto.ReceiveNotificationDTO;
+import br.com.emendes.notification.dto.ConfirmationNotificationDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -15,9 +15,10 @@ import static br.com.emendes.notification.config.constant.AMQPConstant.NOTIFICAT
 public class NotificationListener {
 
   @RabbitListener(queues = {NOTIFICATION_QUEUE_NAME})
-  public void receiveNotification(ReceiveNotificationDTO receiveNotificationDTO) {
-    log.info("EMAIL ::: {}", receiveNotificationDTO.email());
-    log.info("CONTENT ::: {}", receiveNotificationDTO.content());
+  public void receiveNotification(ConfirmationNotificationDTO confirmationNotificationDTO) {
+    log.info("EMAIL ::: {}", confirmationNotificationDTO.addresseeEmail());
+    log.info("CONTENT ::: {}", confirmationNotificationDTO.content());
+    log.info("URI ::: {}", confirmationNotificationDTO.confirmationURI());
 
     // TODO: Delegar para um service realizar a notificação.
   }
